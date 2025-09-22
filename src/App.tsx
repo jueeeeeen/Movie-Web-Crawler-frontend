fb16101121c00d083c2627f2b1b827548a76cbba
MustMark <Superpeerawat.mm@gmail.com>
2025-09-22 22:14:48 +0700

    add member card

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import CrawlPage from './pages/CrawlPage.tsx'
-import MemberPage from './pages/MemberPage.tsx'
+import AboutUsPage from './pages/AboutUsPage.tsx'
 import PaperBackground from './components/Background.tsx'
 
 
 function App() {
-  let page = "crawl"
+  let page = "member"
   return (
     <div className="relative min-h-screen overflow-hidden" id="page-container">
       <PaperBackground />
-      {page === "member" ? <MemberPage />: <CrawlPage/>}
+      {page === "member" ? <AboutUsPage />: <CrawlPage/>}
       <footer className="bg-[url('src/assets/footer.png')] w-full h-16 bg-cover bg-center"></footer>
     </div>
   )
